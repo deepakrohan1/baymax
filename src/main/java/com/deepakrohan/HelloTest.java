03856d4bf628b2d00ea9fec2e0bed9189857bf81
Deepak Rohan Sekar <deepakrohan@hotmail.com>
2017-01-15 12:52:30 -0500

    testing the app

--- src/main/java/com/deepakrohan/HelloTest.java
+++ src/main/java/com/deepakrohan/HelloTest.java
@@ -1,5 +1,53 @@
 package com.deepakrohan;
 
-public class HelloTest {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+
+public class HelloTest implements Speechlet{
+	
+	private static final Logger log = LoggerFactory.getLogger(HelloTest.class);
+
+	@Override
+	public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		log.info(session.getSessionId() +"::Session Id::");
+		
+	}
+
+	@Override
+	public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		return getWelcomeMessage();
+	}
+
+	@Override
+	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	private SpeechletResponse getWelcomeMessage(){
+		String speech = "Hello World, test message";
+		
+		SpeechTe
+		return null;
+		
+	}
 
 }
